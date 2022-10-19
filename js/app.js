9e50abd11069be592261a97f86ba39f330a310c8
tursunova-madina <tmedina0804@gmail.com>
2022-10-19 20:36:31 +0500

    exam

---
+++ js/app.js
@@ -0,0 +1,52 @@
+"use strict";
+
+let billInput = document.querySelector(".bill-input");
+let btns = document.querySelectorAll(".button");
+let custom = document.querySelector(".custom_tip");
+let personTotal = document.querySelector(".bill-input-number");
+let tip = document.querySelector(".tP");
+let totalBill = document.querySelector(".tp");
+let resetBtn = document.querySelector(".reset");
+
+for (let i = 0; i < btns.length; i++) {
+  let btnValue = Number(btns[i].value);
+
+  btns[i].addEventListener("click", () => {
+    let billInputValue = Number(billInput.value);
+    let personTotalValue = Number(personTotal.value);
+    if (personTotalValue==0 && personTotalValue<0) {
+      confirm("It can not be calculated");
+    } else {
+      let tipResult = (billInputValue * (btnValue / 100))/ personTotalValue;
+      tip.textContent = ` $ ${tipResult.toFixed(2)}`;
+      console.log(tipResult);
+      console.log(billInputValue);
+
+      let totalPeople=((billInputValue*(btnValue/100))+billInputValue)/personTotalValue;
+      totalBill.textContent=` $ ${totalPeople.toFixed(2)}`
+    }
+  });
+}
+
+custom.addEventListener("change",()=>{
+  let customValue = Number(custom.value);
+  let billInputValue = Number(billInput.value);
+  let personTotalValue = Number(personTotal.value);
+  let tipResult = (billInputValue *(customValue / 100))/ personTotalValue;
+  tip.textContent = ` $ ${tipResult.toFixed(2)}`;
+  console.log(tipResult);
+  console.log(billInputValue);
+
+  let totalPeople=((billInputValue*(customValue/100))+billInputValue)/personTotalValue;
+  totalBill.textContent=` $ ${totalPeople.toFixed(2)}`
+  
+})
+
+
+resetBtn.addEventListener("click",()=>{
+  tip.textContent="000";
+  totalBill.textContent="000";
+  billInput.value='';
+  personTotal.value='';
+  custom.value='';
+})
---
+++ js/problems.js
@@ -0,0 +1,35 @@
+// 1-masala
+// function sumInput(a) {
+//   let array = [];
+//   let sum = 0;
+//   for (let i = 1; i < a; i++) {
+//     let num = +prompt("son kiriting");
+//     array.push(num);
+//   }
+//   for (let j = 0; j < array.length; j++) {
+//     sum += array[j];
+//   }
+//   console.log(array);
+//   console.log(sum);
+// }
+
+// sumInput(6);
+
+// 2-masala
+
+// let salaries = {
+//   John: 5200,
+//   Mark: 1200,
+//   Mary: 1500,
+// };
+
+// let summm = 0;
+
+// for (const person in salaries) {
+//   if (Object.hasOwnProperty.call(salaries, person)) {
+//     const element = salaries[person];
+//     summm += element;
+//   }
+// }
+
+// console.log(summm);
